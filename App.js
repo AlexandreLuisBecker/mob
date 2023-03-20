16077d386b21c6f051c8d0de745151a890750037
AlexandreLuisBecker <didiao@yahoo.com>
2023-03-20 20:27:05 -0300

    aa

--- App.js
+++ App.js
@@ -1,20 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, Button, FlatList, Image } from 'react-native'
 
-export default function App() {
+import Header from './src/components/Header';
+import DisplayImage from './src/components/DisplayImage';
+import ToDoItem from './src/components/ToDoItem';
+import ToDoInput from './src/components/ToDoInput';
+
+export default function App(){
+  const [tasks,setTasks] = useState([])
+  const [addTasks, setAddTasks] = useState(false)
+
+  const addTaskHandler = taskTitle => {
+    setTasks(currentTasks => [
+      ...currentTasks,
+      {
+        id: Math.random().toString(),
+        value: taskTitle
+      }
+    ])
+    setAddTasks(false)
+  }
+
+  const deleteTaskHandler = taskId => {
+    setTasks(currentTasks => {
+      return currentTasks.filter(task => task.id !== taskId)
+    })
+  }
+
+  const cancelTaskHandler = () => {
+    setAddTasks(false)
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+   <View>
+    <Header title="Lista de Tarefas"></Header>
+    <View style={styles.screen}>
+      <Button title='Adicionar Nova Tarefa!'
+      onPress={() => setAddTasks(true)}
+      ></Button>
+      <ToDoInput
+        visible = {addTasks}
+        onAddTask = {addTaskHandler}
+        onCancel  = {cancelTaskHandlerr}
+      ></ToDoInput>
     </View>
-  );
+   </View>
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  screen: {
+    paddingTop: 70,
+    paddingHorizontal: 70
+  
+  }
+})
